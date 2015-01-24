a824f000a957443485c5cf131dc5131bac5f8844
Tyler Long <tyler.stephen.long@gmail.com>
2015-01-23 22:53:00 -0700

    Update robot-simulator.js

--- javascript/robot-simulator/robot-simulator.js
+++ javascript/robot-simulator/robot-simulator.js
@@ -1,13 +1,11 @@
 (function(){
   function Robot() {
     var validDirections = ["east", "west", "south", "north"]
-
+    
     this.bearing = ""
     this.orient = function(direction)   {
       this.bearing = direction
-
     };
   } 
-  
   module.exports = Robot;
 })();
