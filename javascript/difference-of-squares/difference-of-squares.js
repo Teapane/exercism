35fa3e55d32c0dd7221874367903477700ea36a8
Tyler Long <tyler.stephen.long@gmail.com>
2015-03-04 19:08:09 +0100

    Update difference-of-squares.js
    remove comments

--- javascript/difference-of-squares/difference-of-squares.js
+++ javascript/difference-of-squares/difference-of-squares.js
@@ -1,18 +1,6 @@
  'use strict';
  function Squares(){
     function squareOfSums
-      (225); 
-        return sum; 
-        // for (var i = 1; i < number + 1; i++); 
-        //   sum += i
-
-  
-    
   };
 
 module.exports = Squares;
-
-// for (var i=0; i<5; i++)
-//   {
-//   x=x + "The number is " + i + "<br>";
-//   }
