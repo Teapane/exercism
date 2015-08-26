15068f1b92658fe2cc7a6ae498e93d195ba06e3c
Teapane <tyler.stephen.long@gmail.com>
2015-08-26 14:23:08 -0600

    fix wonky spacing

--- clojure/anagram/anagram.clj
+++ clojure/anagram/anagram.clj
@@ -2,10 +2,10 @@
   (:require [clojure.string :refer [lower-case]]))
 
 (defn- anagram? [word  c]
-    (let [word (lower-case word)
-                  c (lower-case c)]
-          (and (= (sort word) (sort c))
-                        (not= word c))))
+  (let [word (lower-case word)
+        c (lower-case c)]
+    (and (= (sort word) (sort c))
+         (not= word c))))
 
 (defn anagrams-for [word coll]
-    (filter (partial anagram? word) coll))
+  (filter (partial anagram? word) coll))
